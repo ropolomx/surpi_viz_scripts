95c57fbdb684eef520ca6f10ff933dd29c704399
Rodrigo O. Polo <r.ortegapolo@uleth.ca>
2017-02-24 13:49:53 -0700

    Modifications to column names to melt

--- convert_to_Krona_format.py
+++ convert_to_Krona_format.py
@@ -6,13 +6,21 @@ def arguments():
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('surpi-output',
+    parser.add_argument('surpi-output', help='Count table file that is the output of SURPI analysis. Tab-delimited file')
 
     return parser.parse_args()
 
 def melt_counttable(surpi_output):
 
     counttable = pd.read_table(surpi_output)
+    # counttable.columns
+    counttable = pd.melt(counttable, id_vars = ['Species', 'Genus', 'Family'], var_name='Barcode', value_name='Counts')
 
-    counttable = pd.melt(counttable, id_vars = ['Species', 'Genus', 'Species'], var_name='Barcode', value_name='Counts')
+#cols = cols[-3:] + cols [:-3]
+#surpi_sorted = surpi_sorted[cols]
+
+surpi_krona_group = surpi_krona.groupby('Sample')
+
+for name, group in surpi_krona_group:
+        group.to_csv(str(name + ".txt"), columns=header, header=False, index=False)
 
