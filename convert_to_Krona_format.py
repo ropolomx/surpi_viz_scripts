02b6a0cba76dd8c60a4d976134c92441243977b4
Rodrigo O. Polo <r.ortegapolo@uleth.ca>
2017-03-03 01:15:55 -0600

    Added function comments

--- convert_to_Krona_format.py
+++ convert_to_Krona_format.py
@@ -12,16 +12,24 @@ def arguments():
 
 def melt_counttable(surpi_output):
 
+    """Read table with SURPI output and return table in tidy format
+    """
+
     counttable = pd.read_table(surpi_output)
     counttable = pd.melt(counttable, id_vars = ['Species', 'Genus', 'Family'], var_name='Sample', value_name='Counts')
+
+    return counttable
 
 #cols = cols[-1] + cols [:-3]
 #surpi_sorted = surpi_sorted[cols]
 
-surpi_krona_group = counttable.groupby('Sample')
+def group_by_Sample():
+    surpi_krona_group = counttable.groupby('Sample')
 
-# Variable with the desired order of columns to export for
-# Krona visualizations
+def save_to_file():
+    
+    # Variable with the desired order of columns to export for
+    # Krona visualizations
 header = ['Counts','Family','Genus','Species']
 
 for name, group in surpi_krona_group:
